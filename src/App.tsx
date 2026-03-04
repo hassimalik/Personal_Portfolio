deae7df257b11fa7e5ba6cd2dad257728a553a9c
hassaan_aslam <hassaanaslam0321@gmail.com>
2026-03-04 21:58:15 +0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="notfound" element={<NotFound/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
